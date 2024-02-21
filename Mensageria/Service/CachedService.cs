46265f957a555785f12eebff4f7d18c983c41831
Bruno <brunobentocaina@gmail.com>
2024-02-20 21:25:46 -0300

    fix

--- Mensageria/Service/CachedService.cs
+++ Mensageria/Service/CachedService.cs
@@ -5,6 +5,7 @@ namespace Mensageria.Service;
 
 public class CachedService : ICachedService
 {
+
     private readonly IDistributedCache _distributedCache;
     private readonly DistributedCacheEntryOptions _options;
     private static readonly double _absolutExpiration = 60;
